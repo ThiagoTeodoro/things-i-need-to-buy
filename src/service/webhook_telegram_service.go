b38a3151097591506086e5d1241d2164f4ed2dd4
Thiago Teodoro Rodrigues <thiago.teodoro.rodrigues@gmail.com>
2021-02-23 14:29:45 -0300

    Test recovery cell number
    Test recovery cell number.

--- src/service/webhook_telegram_service.go
+++ src/service/webhook_telegram_service.go
@@ -8,19 +8,28 @@ import (
 
 //Update Struct to recive the messages from Telegram this is the Main Struct
 type Update struct {
-	UpdateID int     `json: "update_id"`
-	Message  Message `json: "message"`
+	UpdateID int     `json:"update_id"`
+	Message  Message `json:"message"`
 }
 
 //Message Struct to recive the messages from Telegram this is a secondary Struct
 type Message struct {
-	Text string `json: "text"`
-	Chat Chat   `json: "chat"`
+	Text    string  `json:"text"`
+	Chat    Chat    `json:"chat"`
+	Contact Contact `json:"contact"`
 }
 
 //Chat Struct to recive the messages from Telegram this is a secondary Struct
 type Chat struct {
-	Id int `json: "id"`
+	ID int `json:"id"`
+}
+
+//Contact Struct to revice the contact data from Telegram
+type Contact struct {
+	PhoneNumber string `json:"phone_number"`
+	FirstName   string `json:"first_name"`
+	UserID      int    `json:"user_id"`
+	Vcard       string `json:"vcard"`
 }
 
 //ParseTelegramMessage function to parse the Telegram Messages
