588b5c2e5000e7fc212ca39ebba1fd1d3e648c07
Thiago Teodoro Rodrigues <thiago.teodoro.rodrigues@gmail.com>
2021-02-24 15:29:18 -0300

    Try to see data from Telegram.

--- src/controller/webhook_telegram_controller.go
+++ src/controller/webhook_telegram_controller.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"things.i.need.to.buy/src/service"
@@ -10,6 +12,9 @@ import (
 
 //TelegramWebhook Function to responde the http get on '/telegram/webhook'
 func TelegramWebhook(responseWriter http.ResponseWriter, request *http.Request) {
+
+	requestBody, _ := ioutil.ReadAll(request.Body)
+	log.Printf("Request from Telegram %s", requestBody)
 
 	message, _ := service.TelegramMessageParse(request)
 	text := "Bem vindo ao modulo Javis para armazenamento de lista, ainda estou em construção, em breve estarei em funcionamento."
