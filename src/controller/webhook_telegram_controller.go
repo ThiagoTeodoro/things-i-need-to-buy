e8f02ab36cdb8c30e0be6df5682500efb22ad8ba
Thiago Teodoro Rodrigues <thiago.teodoro.rodrigues@gmail.com>
2021-02-24 15:45:00 -0300

    Test recovy name

--- src/controller/webhook_telegram_controller.go
+++ src/controller/webhook_telegram_controller.go
@@ -3,8 +3,6 @@ package controller
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
 
 	"things.i.need.to.buy/src/service"
@@ -13,11 +11,8 @@ import (
 //TelegramWebhook Function to responde the http get on '/telegram/webhook'
 func TelegramWebhook(responseWriter http.ResponseWriter, request *http.Request) {
 
-	requestBody, _ := ioutil.ReadAll(request.Body)
-	log.Printf("Request from Telegram %s", requestBody)
-
 	message, _ := service.TelegramMessageParse(request)
-	text := "Bem vindo ao modulo Javis para armazenamento de lista, ainda estou em construção, em breve estarei em funcionamento."
+	text := "Bem vindo " + message.From.FirstName + " ao modulo Javis para armazenamento de lista, ainda estou em construção, em breve estarei em funcionamento."
 
 	service.SendToTelegramChat(message.Message.Chat.ID, text)
 
--- src/dto/structs.go
+++ src/dto/structs.go
@@ -4,24 +4,23 @@ package dto
 type Update struct {
 	UpdateID int     `json:"update_id"`
 	Message  Message `json:"message"`
+	From     From    `json:"from"`
+}
+
+//From struct to recive data about the user.
+type From struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	UserName  string `json:"username"` //Unic on intire Telegram
 }
 
 //Message Struct to recive the messages from Telegram this is a secondary Struct
 type Message struct {
-	Text    string  `json:"text"`
-	Chat    Chat    `json:"chat"`
-	Contact Contact `json:"contact"`
+	Text string `json:"text"`
+	Chat Chat   `json:"chat"`
 }
 
 //Chat Struct to recive the messages from Telegram this is a secondary Struct
 type Chat struct {
 	ID int `json:"id"`
-}
-
-//Contact Struct to revice the contact data from Telegram
-type Contact struct {
-	PhoneNumber string `json:"phone_number"`
-	FirstName   string `json:"first_name"`
-	UserID      int    `json:"user_id"`
-	Vcard       string `json:"vcard"`
 }
